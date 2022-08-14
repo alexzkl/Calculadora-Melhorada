f8178ef1ed2a1c6042749e0703115b13f4470dcd
alexzkl <50206980+alexzkl@users.noreply.github.com>
2022-08-14 15:52:06 -0300

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -1,16 +1,22 @@
 # Calculadora-Melhorada
-print ( 'Olá, bem vindo a minha calculadora!' )
-num1  =  int ( input ( 'Qual tabuada você precisa?: ' ))
-passo  =  intervalo ( 1 , 10 + 1 )
-a  = ( 's' )
-b  = ( 'n' )
-para  i  em  passo :
-    print ( '{} x {:2} = {}' . format ( num1 , i , num1  *  i ))
-print ( 'Reponda com s ou n, a próxima pergunta ' )
-per  =  input ( 'Precisa de outra tabuada?' )
-se  por  ==  a :
-    imprimir ( num1 )
-elif  por  ==  b :
-    print ( 'Obrigado por usar minha calculadora!!' )
-mais :
-    print ( 'Não entendi, reponda só com s ou n:' )
+print('Olá, bem vindo a minha calculadora!')
+# Pedindo ao usuário o número da tabuada que deseja
+num1 = int(input('Qual tabuada você precisa?: '))
+#  Definindo o intravalo/range
+passo = range(1,10+1)
+# Criando as váriavel para o input per/pergunta 
+a = ('s')
+b = ('n')
+# Defininfo o for 
+for i in passo:
+    print('{} x {:2} = {}'.format(num1, i, num1 * i))
+# Fazendo as perguntas ao usuário
+print('Reponda com s ou n, a próxima pergunta ')
+per = input('Precisa de outra tabuada? ')
+# Definindo os ifs
+if per == a:
+    print(num1)
+elif per == b:
+    print('Obrigado por usar minha calculadora!!')
+else:
+    print('Não entendi, reponda só com s ou n:')
